c81c07b0b5fa6f98ee2ea183b61fbeba1989a52f
crynom <m.chael@tuta.io>
2025-10-17 22:21:50 +0200

    linked list added

---
+++ structures/linkedList.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+from typing import Any
+from nodes import Node
+
+class LinkedList:
+
+    def __init__(self, data: Any) -> None:
+        self.head = Node(data)
+
+    def __repr__(self) -> str:
+        representation = ''
+        current = self.getHead()
+        while current:
+            representation += f'{current.getData()} -> '
+            current = current.getNext()
+        return representation[:-4]
+    
+    def getHead(self) -> Node|None:
+        return self.head
+    
+    def insert(self, data: Any) -> None:
+        self.head = Node(data, self.head)
+
+    def remove(self, toRemove) -> None:
+        current = self.getHead()
+        if current and current.getData() == toRemove:
+            self.head = current.getNext()
+        else:
+            while current:
+                nextNode = current.getNext()
+                if nextNode and nextNode.getData() == toRemove:
+                    current.setNext(nextNode.getNext())
+                    current = None
+                else:
+                    current = nextNode
+        
+if __name__ == '__main__':
+    ll = LinkedList('c')
+    ll.insert('b')
+    ll.insert('d')
+    ll.insert('a')
+    print(ll)
+    ll.remove('d')
+    print(ll)
